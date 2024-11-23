c4bc248e9150256ecf37c15ff26733a846d23af2
Depeesh Gurung <depeshgurung44@gmail.com>
2024-11-23 14:37:15 +0530

    Create Page of water log completed along with validation

--- Models/DrinkingWater.cs
+++ Models/DrinkingWater.cs
@@ -6,6 +6,11 @@ public class DrinkingWater
 {
     [Key]
     public int Id { get; set; }
+    
+    [Required]
     public DateTime Date { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue, ErrorMessage = "Value of {0} cannot be negative.")]
     public int Quantity { get; set; }
 }
--- Pages/Create.cshtml.cs
+++ Pages/Create.cshtml.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+using Water_Logger.Models;
 
 namespace Water_Logger.Pages
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public required DrinkingWater LoggingData { get; set; }
+        public IConfiguration _config { get; set; }
+
+        public CreateModel(IConfiguration config)
         {
+            _config = config;
+        }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page(); 
+            }
+
+            using var sqlConnection = new SqliteConnection(_config.GetConnectionString("SQLiteConnection"));
+            sqlConnection.Open();
+            SqliteCommand sqlCommand = sqlConnection.CreateCommand();
+            string sqlQuery = @$"INSERT INTO drinking_water
+                (Date, Quantity)
+                VALUES ('{LoggingData.Date.ToString("dd-MM-yyyy")}', {LoggingData.Quantity})";
+
+            sqlCommand.CommandText = sqlQuery;
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            return RedirectToPage("/Index");
         }
     }
 }
